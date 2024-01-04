7d74c9aa771924e5a659411447336637dca06e83
Brahim Biyda <itsbrahimbiyad01@gmail.com>
2024-01-04 01:36:26 +0100

    complete crud operations

--- command-service/src/main/java/com/ecom/commandservice/service/CommandService.java
+++ command-service/src/main/java/com/ecom/commandservice/service/CommandService.java
@@ -3,10 +3,13 @@ package com.ecom.commandservice.service;
 import com.ecom.commandservice.configuration.ApplicationConfiguration;
 import com.ecom.commandservice.entities.Command;
 import com.ecom.commandservice.repository.CommandRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CommandService {
     private CommandRepository commandRepository;
@@ -19,12 +22,37 @@ public class CommandService {
 
     public List<Command> getLastCommands() {
         int days = applicationConfiguration.getCommandesLast();
-        System.out.println("************ CommandLast = "+ days + " ***********");
+        System.out.println("************ CommandLast = " + days + " ***********");
         LocalDate startDate = LocalDate.now().minusDays(days);
         return commandRepository.findLastCommands(startDate);
     }
     public List<Command> getAllCommands(){
         return commandRepository.findAll();
+    }
+    public Optional<Command> getCommandById(Long id) {
+        return commandRepository.findById(id);
+    }
+    public Command addCommand(Command command){
+        return commandRepository.save(command);
+    }
+    public Command updateCommand(Long id, Command updatedCommand) {
+        Optional<Command> existingCommandOptional = getCommandById(id);
+
+        if (existingCommandOptional.isPresent()) {
+            Command existingCommand = existingCommandOptional.get();
+
+            existingCommand.setDescription(updatedCommand.getDescription());
+            existingCommand.setQuantity(updatedCommand.getQuantity());
+            existingCommand.setCreatedAt(updatedCommand.getCreatedAt());
+            existingCommand.setAmount(updatedCommand.getAmount());
+
+            return commandRepository.save(existingCommand);
+        } else {
+            throw new EntityNotFoundException("Command with id " + id + " not found");
+        }
+    }
+    public void deleteCommand(Long id) {
+        commandRepository.deleteById(id);
     }
 
 
--- command-service/src/main/java/com/ecom/commandservice/web/controller/CommandController.java
+++ command-service/src/main/java/com/ecom/commandservice/web/controller/CommandController.java
@@ -3,12 +3,15 @@ package com.ecom.commandservice.web.controller;
 import com.ecom.commandservice.entities.Command;
 import com.ecom.commandservice.repository.CommandRepository;
 import com.ecom.commandservice.service.CommandService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CommandController implements HealthIndicator {
@@ -19,17 +22,45 @@ public class CommandController implements HealthIndicator {
     }
 
     @GetMapping("/lastcommands")
-    public List<Command> commandList(){
-        return commandService.getLastCommands();
+    public ResponseEntity<List<Command>> commandList(){
+        return new ResponseEntity<>(commandService.getLastCommands(), HttpStatus.OK);
     }
     @GetMapping("/commands")
-    public List<Command> allCommands(){
-        return commandService.getAllCommands();
+    public ResponseEntity<List<Command>> allCommands(){
+        return new ResponseEntity<>(commandService.getAllCommands(), HttpStatus.OK);
+    }
+    @GetMapping("/commands/{id}")
+    public ResponseEntity<Command> getCommandById(@PathVariable Long id) {
+        Optional<Command> command = commandService.getCommandById(id);
+
+        return command.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+    @PostMapping("/commands")
+    public ResponseEntity<Command> createCommand(@RequestBody Command command) {
+        Command createdCommand = commandService.addCommand(command);
+        return new ResponseEntity<>(createdCommand, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/commands/{id}")
+    public ResponseEntity<Command> updateCommand(@PathVariable Long id, @RequestBody Command updatedCommand) {
+        try {
+            Command updated = commandService.updateCommand(id, updatedCommand);
+            return new ResponseEntity<>(updated, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/commands/{id}")
+    public ResponseEntity<Void> deleteCommand(@PathVariable Long id) {
+        commandService.deleteCommand(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @Override
     public Health health() {
-        System.out.println("****** Actuator : CommandController health() ");
+        System.out.println("****** Actuator : CommandController health() ******");
         List<Command> commands = commandService.getAllCommands();
         if (commands.isEmpty()) {
             return Health.down().build();
